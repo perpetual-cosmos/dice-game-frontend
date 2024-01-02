4f3c41079ea1dad43bbd2205a5b4430efaed39de
perpetual-cosmos <prataptarunsingh241@gmail.com>
2024-01-02 16:21:42 +0530

    deciding the structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/game" element={<GamePage />} />
+        <Route path="/game-over" element={<GameOverPage />} />
+      </Routes>
+    </Router>
   );
 }
 
