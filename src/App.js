5494025ba52055cb4e9293fce0f8751fb22f9bd8
perpetual-cosmos <prataptarunsingh241@gmail.com>
2024-01-04 18:30:07 +0530

    testing route

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        
         <Route path="/game" element={<GamePage />} />
         <Route path="/game-over" element={<GameOverPage />} />
       </Routes>
