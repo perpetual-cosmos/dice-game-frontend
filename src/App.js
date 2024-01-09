68f6d9c9c5e4f4c9400a5c1fb5b74db470d61efe
perpetual-cosmos <prataptarunsingh241@gmail.com>
2024-01-09 16:01:00 +0530

    updated app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
+import { useEffect } from 'react';
 import HomePage from './pages/HomePage';
 import GamePage from './pages/GamePage';
 import GameOverPage from './pages/GameOverPage';
+import { SocketContext, socket } from './context/socket';
 
 function App() {
+  useEffect(() => {
+    socket.connect();
+    return () => socket.disconnect();
+  }, []);
   return (
+    <SocketContext.Provider value={socket}>
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
@@ -14,6 +19,7 @@ function App() {
         <Route path="/game-over" element={<GameOverPage />} />
       </Routes>
     </Router>
+    </SocketContext.Provider>
   );
 }
 
