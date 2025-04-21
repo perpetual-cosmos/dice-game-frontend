d2a24693daabd2546ff9b72e67b737a6bd9ca6db
perpetual-cosmos <prataptarunsingh241@gmail.com>
2025-04-21 11:48:17 +0530

    server url

--- src/context/socket.js
+++ src/context/socket.js
@@ -1,7 +1,7 @@
 import { createContext } from 'react';
 import { io } from 'socket.io-client';
 
-export const socket = io(process.env.REACT_APP_SOCKET_URL || 'http://localhost:3001', {
+export const socket = io(process.env.REACT_APP_SOCKET_URL || 'https://dice-game-server.onrender.com', {
   autoConnect: false
 });
 
