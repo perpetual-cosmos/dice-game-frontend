b221cb4133da1807dd20117d250ec505271738af
perpetual-cosmos <prataptarunsingh241@gmail.com>
2025-05-08 13:57:14 +0530

    decide new route and added on app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import HomePage from './pages/HomePage';
 import GamePage from './pages/GamePage';
 import GameOverPage from './pages/GameOverPage';
+import LeaderboardPage from './pages/LeaderboardPage';
 import { SocketContext, socket } from './context/socket';
 
 function App() {
@@ -16,7 +17,9 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/game" element={<GamePage />} />
+        <Route path="/game/:roomId" element={<GamePage />} />
         <Route path="/game-over" element={<GameOverPage />} />
+        <Route path="/leaderboard" element={<LeaderboardPage />} />
       </Routes>
     </Router>
     </SocketContext.Provider>
